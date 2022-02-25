ffe43966c081ecfa6ee4a5e67d0ecc6722cda715
Jesse Benevides <jessebenevides@gmail.com>
2022-02-25 20:12:29 -0300

    [MODELS] Add função para buscar as vendas pelo Id - corrige querry da getAll

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,12 +1,31 @@
+const { serializeSales } = require('../utils/serialize');
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.sales;';
+  const query = `
+    SELECT s_p.sale_id, s_p.product_id, s_p.quantity, s.date
+    FROM StoreManager.sales_products AS s_p
+    INNER JOIN StoreManager.sales AS s
+    ON s.id = s_p.sale_id;`;
   const [sales] = await connection.execute(query);
 
-  return sales;
+  return sales.map(serializeSales);
+};
+
+const findById = async (id) => {
+  const query = `
+    SELECT s.date, s_p.product_id, s_p.quantity
+    FROM StoreManager.sales AS s
+    INNER JOIN StoreManager.sales_products AS s_p
+    ON s.id = s_p.product_id
+    WHERE s_p.sale_id = ?;`;
+
+  const [sale] = await connection.execute(query, [id]);
+
+  return sale.map(serializeSales);
 };
 
 module.exports = {
   getAll,
+  findById,
 };
