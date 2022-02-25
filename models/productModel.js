9f3f5673839073fe03bbc5929f9712fab4fb6fca
Jesse Benevides <jessebenevides@gmail.com>
2022-02-25 18:57:32 -0300

    [MODELS] Adiciona função para buscar um produto por id no banco

--- models/productModel.js
+++ models/productModel.js
@@ -7,6 +7,14 @@ const getAll = async () => {
   return producs;
 };
 
+const findById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [product] = await connection.execute(query, [id]);
+
+  return product;
+};
+
 module.exports = {
   getAll,
+  findById,
 };
