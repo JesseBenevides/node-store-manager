18a4fee0344e0ba08520e769bf7d9f9507a2bc45
Jesse Benevides <jessebenevides@gmail.com>
2022-02-25 20:13:48 -0300

    [SERVICES] Add a funão para retornar as vendas por id - muda formato do retorno

--- services/salesService.js
+++ services/salesService.js
@@ -3,9 +3,19 @@ const salesModel = require('../models/salesModel');
 const getAll = async () => {
   const sales = await salesModel.getAll();
 
-  return sales;
+  return { status: 200, data: sales };
+};
+
+const findById = async (id) => {
+  const sales = await salesModel.findById(id);
+
+  if (sales.length === 0) {
+    return { status: 404, message: 'Sale not found' };
+  }
+  return { status: 200, data: sales };
 };
 
 module.exports = {
   getAll,
+  findById,
 };
