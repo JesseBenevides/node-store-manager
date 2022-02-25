df8be9deb6822cdac449ad961d33f7c94c78c6e9
Jesse Benevides <jessebenevides@gmail.com>
2022-02-25 18:58:49 -0300

    [SERVICES] Adiciona função para retornar busca de produtos por id - muda formato do retorno

--- services/productService.js
+++ services/productService.js
@@ -1,10 +1,20 @@
 const productModel = require('../models/productModel');
 
 const getAll = async () => {
-  const producs = await productModel.getAll();
-  return producs;
+  const products = await productModel.getAll();
+  return { status: 200, data: products };
+};
+
+const findById = async (id) => {
+  const product = await productModel.findById(id);
+
+  if (product.length === 0) {
+    return { status: 404, message: 'Product not found' };
+  }
+  return { status: 200, data: product[0] };
 };
 
 module.exports = {
   getAll,
+  findById,
 };
