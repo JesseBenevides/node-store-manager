f9c69c221a029f7e5e06b554521edc8c6e3e0914
Jesse Benevides <jessebenevides@gmail.com>
2022-02-27 19:49:16 -0300

    [MODELS] Add função findByName e create em productModel

--- models/productModel.js
+++ models/productModel.js
@@ -8,13 +8,27 @@ const getAll = async () => {
 };
 
 const findById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
   const [product] = await connection.execute(query, [id]);
 
   return product;
 };
 
+const findByName = async (name) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE name=?;';
+  const [product] = await connection.execute(query, [name]);
+  return product;
+};
+
+const create = async (name, quantity) => {
+  const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?);';
+  const [response] = await connection.execute(query, [name, quantity]);
+  return { id: response.insertId, name, quantity };
+};
+
 module.exports = {
   getAll,
   findById,
+  findByName,
+  create,
 };
