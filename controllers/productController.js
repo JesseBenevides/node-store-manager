61843550936b850325f3039a88579f7d01faf0df
Jesse Benevides <jessebenevides@gmail.com>
2022-02-27 19:50:54 -0300

    [CONTROLLERS] Add função para criar um novo produto

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,7 +23,22 @@ const findById = async (req, res, next) => {
   }
 };
 
+const create = async (req, res, next) => {
+  try {
+    const { name, quantity } = req.body;
+    const result = await productService.create(name, quantity);
+
+    if (result.data) {
+      return res.status(result.status).json(result.data);
+    }
+    next(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAll,
   findById,
+  create,
 };
