2915573c0a210ec95f66a45b6430dd8509b6c8c5
Jesse Benevides <jessebenevides@gmail.com>
2022-03-01 17:05:07 -0300

    [SERVICES] Add função para atualizar os dados de produto, se o id for válido

--- services/productService.js
+++ services/productService.js
@@ -16,7 +16,7 @@ const findById = async (id) => {
 
 const create = async (name, quantity) => {
   const product = await productModel.findByName(name);
-  console.log(product);
+
   if (product.length !== 0) {
     return { status: 409, message: 'Product already exists' };
   }
@@ -24,8 +24,22 @@ const create = async (name, quantity) => {
   return { status: 201, data: newProduct };
 };
 
+const update = async ({ name, quantity, id }) => {
+  const product = await productModel.findById(id);
+  console.log(product);
+  if (product.length === 0) {
+    return { status: 404, message: 'Product not found' };
+  }
+
+  const result = await productModel.update({ name, quantity, id });
+  console.log(result);
+
+  return { status: 200, data: { name, quantity, id } };
+};
+
 module.exports = {
   getAll,
   findById,
   create,
+  update,
 };
