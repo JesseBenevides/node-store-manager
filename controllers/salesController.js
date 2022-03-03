cf6d06fb4d445f953f12a9cbe121680f4effed6c
Jesse Benevides <jessebenevides@gmail.com>
2022-03-03 16:46:33 -0300

    [CONTROLLERS] Add funções update e exclude vendas

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -36,8 +36,39 @@ const create = async (req, res, next) => {
   }
 };
 
+const update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await salesService.update(id, req.body);
+
+    if (result.data) {
+      return res.status(result.status).json(result.data);
+    }
+    next(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const exclude = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await salesService.exclude(id);
+
+    if (result.data) {
+      return res.status(result.status).end();
+    }
+    
+    next(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAll,
   findById,
   create,
+  update,
+  exclude,
 };
