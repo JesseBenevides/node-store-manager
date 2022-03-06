1617f65fae0992fecea24a0229515f1ca490f6a4
Jesse Benevides <jessebenevides@gmail.com>
2022-03-06 20:37:09 -0300

    [TESTS] [productController] Testa a função getAll

---
+++ test/unit/controllers/productController.test.js
@@ -0,0 +1,61 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productController = require('../../../controllers/productController');
+const productService = require('../../../services/productService');
+
+
+describe('Product Controller', () => {
+  const request = {};
+  const response = {};
+  let next = () => {};
+  before(() => {
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
+  });
+  describe('GetAll - Busca todos os produtos', () => {
+    describe('Quando não há produtos cadastrados', () => {
+      const serviceResponse = { status: 200, data: [] };
+      before(() => {
+        sinon.stub(productService, 'getAll').resolves(serviceResponse);
+      });
+      after(() => {
+        productService.getAll.restore();
+      });
+
+      it('A resposta da requisição possui o status 200', async () => {
+        await productController.getAll(request, response);
+        expect(response.status.calledWith(200)).to.be.true;
+      });
+      it('A requisição responde com um array vazio no json', async () => {
+        await productController.getAll(request, response);
+        expect(response.json.calledWith([])).to.be.true;
+      });
+    });
+    describe('Quando há produtos cadastrados', () => {
+      const serviceResponse = { status: 200, data: [{
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10
+      }]};
+      before(() => {
+        sinon.stub(productService, 'getAll').resolves(serviceResponse);
+      });
+      after(() => {
+        productService.getAll.restore();
+      });
+      it('A resposta da requisição possui o status 200', async () => {
+        await productController.getAll(request, response);
+        expect(response.status.calledWith(200)).to.be.true;
+      });
+      it('A requisição responde com um array no json', async () => {
+        await productController.getAll(request, response);
+        expect(response.json.calledWith(sinon.match.array)).to.be.true;
+      });
+      it('A requisição responde com o array recebido pelo services', async () => {
+        await productController.getAll(request, response);
+        expect(response.json.calledWith(serviceResponse.data)).to.be.true;
+      });
+    });
+  });
+});
