b310a545b94a7dc5745fddb5efffc4f6bce974fb
Jesse Benevides <jessebenevides@gmail.com>
2022-03-08 11:00:53 -0300

    [TESTS] [productModel] Testa a função findByName

--- test/unit/models/productModel.js
+++ test/unit/models/productModel.js
@@ -126,4 +126,59 @@ describe('Product Models', () => {
       });
     });
   });
+
+  describe('FindByName - Busca um produto pelo nome', () => {
+    describe('Quando o produto não é encrontrado', () => {
+      before(() => {
+        const result = [[]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await productModel.findByName();
+        expect(result).to.be.an('array');
+      });
+  
+      it('Retorna um array vazio', async () => {
+        const result = await productModel.findByName();
+        expect(result).to.be.empty;
+      });
+    });
+
+    describe('Quando o produto é encontrado', () => {
+      before(() => {
+        const result = [[
+          {
+            "id": 1,
+            "name": "produto A",
+            "quantity": 10
+          }
+        ]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await productModel.findByName();
+        expect(result).to.be.an('array');
+      });
+      it('O array possui um item', async () => {
+        const result = await productModel.findByName();
+        expect(result.length).to.be.equal(1);
+      });
+      it('O item do array é um objeto', async () => {
+        const [item] = await productModel.findByName();
+        expect(item).to.be.an('object');
+      });
+      it('Os item possui as propriedades "id", "name" e "quantity" ', async () => {
+        const [item] = await productModel.findByName();
+        expect(item).to.include.all.keys('id', 'name', 'quantity');
+      });
+    });
+  });
 });
