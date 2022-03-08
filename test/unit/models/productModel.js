15384e51f4e1d27c80a83108d8c4ddb478039908
Jesse Benevides <jessebenevides@gmail.com>
2022-03-08 10:53:26 -0300

    [TESTS] [productModel] Testa a função findById

--- test/unit/models/productModel.js
+++ test/unit/models/productModel.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 const productModel = require('../../../models/productModel');
 const connection = require('../../../models/connection');
 
-describe.skip('Product Models', () => {
+describe('Product Models', () => {
   describe('GetAll - Busca todos os produtos do banco', () => {
     describe('Quando não há produtos cadastrados', () => {
       before(() => {
@@ -69,6 +69,61 @@ describe.skip('Product Models', () => {
         expect(item.quantity).to.be.a('number');
       });
   
+    });
+  });
+
+  describe('FindById - Busca um produto pelo id', () => {
+    describe('Quando o produto não é encrontrado', () => {
+      before(() => {
+        const result = [[]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await productModel.findById();
+        expect(result).to.be.an('array');
+      });
+  
+      it('Retorna um array vazio', async () => {
+        const result = await productModel.findById();
+        expect(result).to.be.empty;
+      });
+    });
+
+    describe('Quando o produto é encontrado', () => {
+      before(() => {
+        const result = [[
+          {
+            "id": 1,
+            "name": "produto A",
+            "quantity": 10
+          }
+        ]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await productModel.findById();
+        expect(result).to.be.an('array');
+      });
+      it('O array possui um item', async () => {
+        const result = await productModel.findById();
+        expect(result.length).to.be.equal(1);
+      });
+      it('O item do array é um objeto', async () => {
+        const [item] = await productModel.findById();
+        expect(item).to.be.an('object');
+      });
+      it('Os item possui as propriedades "id", "name" e "quantity" ', async () => {
+        const [item] = await productModel.findById();
+        expect(item).to.include.all.keys('id', 'name', 'quantity');
+      });
     });
   });
 });
