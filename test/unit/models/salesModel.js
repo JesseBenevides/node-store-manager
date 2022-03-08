bb99d27e60e33ea96ade02b2a7cc3e16d6ef52eb
Jesse Benevides <jessebenevides@gmail.com>
2022-03-08 15:11:41 -0300

    [TESTS] [selesModel] Testa a função findById

--- test/unit/models/salesModel.js
+++ test/unit/models/salesModel.js
@@ -65,4 +65,66 @@ describe('Sales Model', () =>{
       });
     });
   });
+
+  describe('FindById - Busca os dados de uma venda pelo id', () => {
+    const id = 1;
+    describe('Quando a venda não é encontrada', () => {
+      before(() => {
+        const result = [[]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await salesModel.findById(id);
+        expect(result).to.be.an('array');
+      });
+      it('Retorna um array vazio', async () => {
+        const result = await salesModel.findById(id);
+        expect(result).to.be.empty;
+      });
+    });
+    describe('Quando a venda é encontrada', () => {
+      before(() => {
+        const result = [[
+          {
+            "saleId": 1,
+            "date": "2021-09-09T04:54:29.000Z",
+            "productId": 1,
+            "quantity": 2
+          },
+          {
+            "saleId": 1,
+            "date": "2021-09-09T04:54:54.000Z",
+            "productId": 2,
+            "quantity": 2
+          }
+        ]];
+        sinon.stub(connection, 'execute').resolves(result);
+      });
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Retorna um array', async () => {
+        const result = await salesModel.findById(id);
+        expect(result).to.be.an('array');
+      });
+      it('Retorna um array com elementos', async () => {
+        const result = await salesModel.findById(id);
+        expect(result).to.be.not.empty;
+      });
+      it('Retorna um array de objetos', async () => {
+        const [item] = await salesModel.findById(id);
+        expect(item).to.be.an('object');
+      });
+      it('Os objetos possuem as propriedades corretas ',
+        async () => {
+          const [item] = await salesModel.getAll();
+          expect(item).to.include.all.keys('saleId', 'date', 'productId', 'quantity');
+      });
+    });
+  });
 });
